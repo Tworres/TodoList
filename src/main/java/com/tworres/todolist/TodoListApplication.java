ef24a87066b48c1eb311f81d376288bd60abb48c
Guilherme Torres <36198151+Tworres@users.noreply.github.com>
2025-08-02 13:56:50 -0300

    chore: base java

---
+++ src/main/java/com/tworres/todolist/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.tworres.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tworres/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tworres.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
